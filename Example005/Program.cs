0c7af198f13f822566cba65bd725f9251d7859b3
SlenderSmile <andrei.ceb.2000@mail.ru>
2023-02-08 21:27:42 +0300

    add Example, add uName Masha

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter username");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Yey, it's you !");
+}
+else
+{
+    Console.WriteLine("Hi, "+ username);
+}
