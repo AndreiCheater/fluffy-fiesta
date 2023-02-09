50cdf28957470f056c32c240085e90171802af04
SlenderSmile <andrei.ceb.2000@mail.ru>
2023-02-09 20:05:40 +0300

    add Sobaka

---
+++ Sobaka1/Program.cs
@@ -0,0 +1,19 @@
+﻿int count = 0, firstFriendSpeed = 1, 
+    secondFriendSpeed = 2, dogSpeed = 5, friend = 2;
+double time, distance = 15;
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        time = distance/ (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/ (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed)* time;
+    count++;
+}
+Console.WriteLine(count);
