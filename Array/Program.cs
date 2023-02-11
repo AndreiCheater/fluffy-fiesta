3a5752cf9532b7129f343e3e8b3b988254407c1a
SlenderSmile <andrei.ceb.2000@mail.ru>
2023-02-11 19:24:08 +0300

    add Array

---
+++ Array/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int resoult  = arg1;
+    if(arg2 > resoult) resoult = arg2;
+    if (arg3 > resoult) resoult = arg3;
+    return resoult;
+}
+int[] array = { 11, 21, 31, 45, 15, 61, 17, 18, 19};
+array[0] = 12;
+
+int Max
+
+Console.WriteLine(array[0]);
